34e4660635dd6d0bb00e15d9b284c4b6278af395
Gary A. Stafford <garystafford@rochester.rr.com>
2019-04-15 21:14:47 -0400

    Update README

--- greeting.pb.gw.go
+++ greeting.pb.gw.go
@@ -99,7 +99,7 @@ func RegisterGreetingServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 }
 
 var (
-	pattern_GreetingService_Greeting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"api", "greeting"}, ""))
+	pattern_GreetingService_Greeting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "greeting"}, ""))
 )
 
 var (
