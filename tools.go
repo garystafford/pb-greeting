34a7cae8868fdb390112f0a087b7b3cfe9ec82d0
Gary A. Stafford <gary.a.stafford@gmail.com>
2021-06-29 09:42:37 -0400

    Working version of gRPC with v2 gRPC Gateway

--- tools.go
+++ tools.go
@@ -2,6 +2,16 @@
 
 package tools
 
+// v1.16.0 grpc-gateway
+// https://github.com/grpc-ecosystem/grpc-gateway/tree/094a6fe78b3ca888297d090185cdf30f0e42e157
+//import (
+//	_ "github.com/golang/protobuf/protoc-gen-go"
+//	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+//	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+//)
+
+// v2 grpc-gateway
+//https://github.com/grpc-ecosystem/grpc-gateway
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
